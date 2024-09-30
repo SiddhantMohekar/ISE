a214246d729fdd64ef6ed048efa29dbf4be04892
SiddhantMohekar <133594916+SiddhantMohekar@users.noreply.github.com>
2024-10-01 00:14:02 +0530

    Update UserController.java

--- src/main/java/com/ise2/is2/Controller/UserController.java
+++ src/main/java/com/ise2/is2/Controller/UserController.java
@@ -3,6 +3,7 @@ package com.ise2.is2.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller 
